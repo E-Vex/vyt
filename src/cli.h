8290540b9f4ed312e7963095422dcb97315f6257
elia <raiedaleve@gmail.com>
2026-08-27 16:24:29 +0300

    feat(cli): add mode_t enum and prepare main for CLI parsing
    - Add mode_t enum in cli.h to represent selectable modes
      (music, video, search, help, none)
    - Include cli.h in main.c and update main() to accept
      argc/argv in preparation for argument parsing

--- src/cli.h
+++ src/cli.h
@@ -3,6 +3,16 @@
 
 #include <stdio.h>
 
+/* The mode the user requested on the command line. */
+typedef enum
+{
+    MODE_NONE,   /* nothing selected */
+    MODE_MUSIC,  /* -m URL  */
+    MODE_VIDEO,  /* -v URL  */
+    MODE_SEARCH, /* -s QUERY */
+    MODE_HELP    /* -h */
+} mode_t;
+
 void cli_print_usage(FILE *stream);
 void cli_print_help(void);
 
