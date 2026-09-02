9262f7d4e4710629659ae686c149c47222909782
E-Vex <raiedaleve@gmail.com>
2026-09-02 23:27:24 +0300

    feat(player): add player_play_video
    Add player_play_video() building an mpv argv vector (without
    --no-video) and delegating to proc_run(), mirroring
    player_play_music().

--- src/player.c
+++ src/player.c
@@ -12,3 +12,13 @@ int player_play_music(const char *url)
         NULL};
     return proc_run("mpv", argv);
 }
+
+int player_play_video(const char *url)
+{
+    char *argv[] = {
+        "mpv",
+        (char *)url,
+        NULL
+    };
+    return proc_run("mpv", argv);
+}
--- src/player.h
+++ src/player.h
@@ -2,5 +2,6 @@
 #define PLAYER_H
 
 int player_play_music(const char *url);
+int player_play_video(const char *url);
 
 #endif /* PLAYER_H */
